9f566fe89e2673804f4cdd4c7fa07f61fc946352
rembov <i7888@mail.ru>
2024-10-13 22:27:37 +0300

    Создал функцию проверки победы

--- kre.py
+++ kre.py
@@ -9,3 +9,28 @@ def draw_board(board):
         print("-" * 13)
 
 
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except ValueError:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer - 1]) not in "XO":
+                board[player_answer - 1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
+
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
