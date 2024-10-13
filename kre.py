91326b27e4563169e278ae76f39c60a51ce19984
rembov <i7888@mail.ru>
2024-10-13 22:39:00 +0300

    Создал игровое поле

--- kre.py
+++ kre.py
@@ -1,32 +1,8 @@
+board = list(range(1, 10))
 
 
-
-def main():
-    while True:
-        board = list(range(1, 10))  # Reset the board
-        counter = 0
-        win = False
-        while not win:
-            draw_board(board)
-            if counter % 2 == 0:
-                take_input("X")
-            else:
-                take_input("O")
-            counter += 1
-
-            tmp = check_win(board)
-            if tmp:
-                print(tmp, "выиграл!")
-                win = True
-                break
-            if counter == 9:
-                print("Ничья!")
-                break
-        draw_board(board)
-
-        restart = input("Начать новую игру? (y/n): ")
-        if restart.lower() != 'y':
-            break
-
-if __name__ == "__main__":
-    main()
+def draw_board(board):
+    print("-" * 13)
+    for i in range(3):
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
